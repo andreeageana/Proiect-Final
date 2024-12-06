086c47d03b0c30276e53750292332f784fd0256b
PC <modol.andreea23@gmail.com>
2024-12-06 21:55:36 +0200

    first update

---
+++ src/test/java/data/JAXBException.java
@@ -0,0 +1,4 @@
+package data;
+
+public class JAXBException extends Exception {
+}
---
+++ src/test/java/tests/SearchTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import POJO.SearchModel;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.SearchPage;
+
+public class SearchTest extends BaseTest {
+    @Test(dataProvider = "searchValidDataProvider", dataProviderClass = data.SearchDataProvider.class)
+    public void searchValidTest(SearchModel searchModel) {
+        setUP();
+        driver.navigate().to(baseURL);
+        SearchPage searchPage = new SearchPage(driver);
+        System.out.println(searchModel);
+        searchPage.search(searchModel.getSearchPhrase());
+        System.out.println("Verify valid search results");
+        Assert.assertTrue(searchPage.verifyValidSearchResults(searchModel.getSearchPhrase()));
+    }
+
+    private void setUP() {
+    }
+
+    @Test(dataProvider = "searchInvalidDataProvider", dataProviderClass = data.SearchDataProvider.class)
+    public void searchNegativeTest(SearchModel searchModel) {
+        setUP();
+        driver.navigate().to(baseURL);
+        SearchPage searchPage = new SearchPage(driver);
+        System.out.println(searchModel);
+        searchPage.search(searchModel.getSearchPhrase());
+        System.out.println("Verify invalid Search test");
+        Assert.assertTrue(searchPage.verifyNegativeSearchMessage(searchModel.getSearchPhrase()));
+    }
+
+}
